65e2e59b3b58c7fe9d5368c4dce033953ca03f36
Andrew Leonard <AndrewCLeonard@fastmail.com>
2023-01-17 21:20:20 -0800

    finished 2.1

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+// react-dom
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div>Hello React</div>;
+};
+
+
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(<App />);
